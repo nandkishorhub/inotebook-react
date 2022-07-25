1cbd2f00adfb3facab30808feb7e177362ffcce0
Nandkishor <nandkishorcdacacts@gmail.com>
2022-07-25 17:01:56 +0530

    models and routes

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
-const connectToMongo = require('./db');
+const connectToMongo = require("./db");
 connectToMongo();
 
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+// it requires in order to use req.body in respctive routes file
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
